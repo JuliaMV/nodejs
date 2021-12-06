a0c6e85a4e5db9af960337e0fc52be83561a1738
Yuliya_Miatlionak <Yuliya_Miatlionak@epam.com>
2021-12-06 23:53:41 +0300

    add task 5.2

--- src/middlewares/catchApiError.ts
+++ src/middlewares/catchApiError.ts
@@ -1,8 +1,9 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import status from 'http-status';
 
 import { ApiHandler } from '../types';
 import { ApiError } from '../errors';
+import logger from '../logger';
 
 export const catchApiError = <TReq>(fn: ApiHandler<TReq>) => async (req: TReq, res: Response, next: NextFunction) => {
   try {
@@ -12,6 +13,7 @@ export const catchApiError = <TReq>(fn: ApiHandler<TReq>) => async (req: TReq, r
     if (errStatus) {
       res.status(errStatus).send(message);
     } else {
+      logger.log('error', `Unhandled API Error at ${(req as unknown as Request).originalUrl}`);
       res.sendStatus(status.INTERNAL_SERVER_ERROR);
     }
   }
